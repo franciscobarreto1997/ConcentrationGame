a3cf836f78f63174df4154aa07eda3186f458b79
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-10-21 18:50:19 +0100

    started the concentration game with basic setup done

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,15 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0 {
+        didSet {
+            flipsLabel.text = "Flips: \(flipCount)"
+        }
     }
+    
+    var emojiArray = ["👻","🎃","👻","🎃"]
+    
+    @IBOutlet var cards: [UIButton]!
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var flipsLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cards.index(of: sender) {
+           flipCard(withEmoji: emojiArray[cardNumber], on: sender)
+        } else {
+            print("Chosen card was not in the cards array")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+            button.setTitle("", for: .normal)
+        } else {
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            button.setTitle(emoji, for: .normal)
+        }
     }
 
 
