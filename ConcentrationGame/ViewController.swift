b9caf5c206033ad720bd722500127f56941c4350
Francisco Barreto <franciscoantunesbarreto1997@gmail.com>
2018-10-21 12:19:36 +0100

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Francisco Barreto on 21/10/18.
+//  Copyright © 2018 Francisco Barreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
